acda46f636f2dcb70fcf3717bddcdd3924e5bc7a
Murphy <pilibmurphy@gmail.com>
2026-01-28 13:42:01 +0000

    Getting linting setup for the project

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,47 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import prettierPlugin from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+import react from "eslint-plugin-react";
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  { ignores: ["dist", ".pnp.cjs", ".pnp.loader.mjs"] },
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
+        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: "module",
       },
     },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier: prettierPlugin,
+    },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+
+      "react/jsx-uses-vars": "error",
+
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+
+      // Prettier as an ESLint rule (fails CI if formatting is wrong)
+      "prettier/prettier": "error",
+
+      ...eslintConfigPrettier.rules,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
-])
+];
